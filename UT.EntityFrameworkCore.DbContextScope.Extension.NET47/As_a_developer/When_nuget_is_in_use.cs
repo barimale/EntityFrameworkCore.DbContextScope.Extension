27d1f638e7b1ffa5389835b561527162eb038bd6
barimale <mateusz.wolnica@gmail.com>
2020-02-06 22:12:53 +0100

    Initial commit.

---
+++ UT.EntityFrameworkCore.DbContextScope.Extension.NET47/As_a_developer/When_nuget_is_in_use.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace UT.EntityFrameworkCore.DbContextScope.Extension.NET47
+{
+    public class When_nuget_is_in_use
+    {
+        [Test]
+        public void I_d_like_to_have_bindings_loaded_correctly()
+        {
+            //given
+
+            //when
+
+            //then
+            Assert.IsNotNull(null);
+        }
+    }
+}
